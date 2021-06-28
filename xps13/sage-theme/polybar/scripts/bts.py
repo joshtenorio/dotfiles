9ee132731db9d9f425757a9ba5eb660547aa0fcb
joshtenorio <joshliam09@gmail.com>
2021-06-27 23:07:25 -0700

    added sage theme files

---
+++ xps13/sage-theme/polybar/scripts/bts.py
@@ -0,0 +1,10 @@
+# a script that outputs a random line from a bts song
+# author: joshua tenorio
+
+import sys
+import json
+import random
+
+#print("But time waits for nobody, Doesn't even say sorry...")
+print("Like an echo in the forest this day will come back around")
+sys.exit(0)
---
+++ xps13/sage-theme/polybar/scripts/polybts.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python3 .config/polybar/scripts/bts.py
+
