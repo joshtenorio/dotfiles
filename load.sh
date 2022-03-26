fff76219562bafc807e1d27aef385ed64d8d3d63
Joshua Tenorio <joshliam09@gmail.com>
2022-03-25 20:12:59 -0700

    uwu

--- load.sh
+++ load.sh
@@ -3,5 +3,11 @@ echo "Input theme to load: "
 read theme
 
 echo "Loading " $theme " theme..."
-
-
+cd $theme
+cp -rf $theme/bspwm $HOME/.config/bspwm
+cp -rf $theme/sxhkd $HOME/.config/sxhkd
+cp -rf $theme/ranger $HOME/.config/ranger
+cp -rf $theme/alacritty $HOME/.config/alacritty
+cp -rf $theme/zathura $HOME/.config/zathura
+cp -rf $theme/polybar $HOME/.config/polybar
+cp ../.vimrc $HOME/.vimrc
