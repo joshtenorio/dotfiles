d0db1e77595a4117664fc11626f57a4a179ceec5
Joshua Tenorio <joshliam09@gmail.com>
2022-03-25 20:14:00 -0700

    added 'check'

--- load.sh
+++ load.sh
@@ -2,6 +2,9 @@
 echo "Input theme to load: "
 read theme
 
+echo "this will overwrite. are you sure? ctrl + c if you dont want to"
+read uwu
+
 echo "Loading " $theme " theme..."
 cd $theme
 cp -rf $theme/bspwm $HOME/.config/bspwm
